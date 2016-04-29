0bdd1862af0ffd50f7060b746e2a609fce7e064f
ghost <ghost@konpa.ku>
2016-04-29 21:56:05 +0000

    0.0.2 - 26851af05ec924a6261a83b0f251e7876c95e6fd

--- implementors/std/io/trait.Read.js
+++ implementors/std/io/trait.Read.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['pipe'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='pipe/struct.PipeReader.html' title='pipe::PipeReader'>PipeReader</a>",];
+implementors['pipe'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='pipe/struct.PipeReader.html' title='pipe::PipeReader'>PipeReader</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
