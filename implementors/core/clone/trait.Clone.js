b30c4c59687c61bae37ee46460de7e2fb45cb729
ghost <ghost@konpa.ku>
2017-12-23 18:13:39 +0000

    0.0.3 - 8b39f33ad0ec2cc4d1b2bcd301369b2c2408729a

--- implementors/core/clone/trait.Clone.js
+++ implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['pipe'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='pipe/struct.PipeWriter.html' title='pipe::PipeWriter'>PipeWriter</a>",];
+implementors["pipe"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"pipe/struct.PipeWriter.html\" title=\"struct pipe::PipeWriter\">PipeWriter</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
